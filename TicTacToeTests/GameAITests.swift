5b37b33b2dc05858c204e75d2815f217435a2541
blubrandi <blubrandi0@gmail.com>
2019-12-11 11:12:15 -0700

    Created GameAITests file to test GameAI logic.  Removed boilerplate functions.

---
+++ TicTacToeTests/GameAITests.swift
@@ -0,0 +1,15 @@
+//
+//  GameAITests.swift
+//  TicTacToeTests
+//
+//  Created by Brandi on 12/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameAITests: XCTestCase {
+
+
+
+}
