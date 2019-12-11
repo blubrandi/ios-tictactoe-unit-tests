189a930c1437106b4a7ebb4ee1ff081188729d99
blubrandi <blubrandi0@gmail.com>
2019-12-11 12:36:41 -0700

    testing diagnoal tests

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -14,36 +14,59 @@ func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
     // check verticals
     // refer to 52:15 for explanation of this loop
 
-    //    for x in 0..<3 {
-//        var numMarks = 0
-//        for y in 0..<3 {
-//            if board[(x,y)] == player {
-//                numMarks += 1
-//            }
-//        }
-//        if numMarks == 3 {
-//            return true
-//        }
-//    }
-    
-    
-    // check horizontals
-    
-    for y in 0..<3 {
-        var numMarks = 0
-        
         for x in 0..<3 {
+        var numMarks = 0
+        for y in 0..<3 {
             if board[(x,y)] == player {
                 numMarks += 1
             }
         }
-        
         if numMarks == 3 {
             return true
         }
     }
     
-    return false
     
+     // check horizontals
+    
+    for y in 0..<3 {
+        var numMarks = 0
+        for x in 0..<3 {
+            if board[(x,y)] == player {
+                numMarks += 1
+            }
+        }
 
+        if numMarks == 3 {
+            return true
+        }
+    }
+
+    return false
+
+    // check diagonals
+    
+    let ltr: [Coordinate] = [(0,0), (1,1), (2,2)]
+    var numMatches = 0
+    for coordinate in ltr {
+        if board[coordinate] == player {
+            // board receives coordinates, and if it matches, it will increment
+            numMatches += 1
+        }
+    }
+    
+    if numMatches == 3 { return true }
+    
+    let rtl: [Coordinate] = [(2,0), (1,1), (0,2)]
+    numMatches = 0
+    for coordinate in rtl {
+        if board[coordinate] == player {
+            // board receives coordinates, and if it matches, it will increment
+            numMatches += 1
+        }
+    }
+
+    if numMatches == 3 { return true }
+    
+    return false
 }
--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -109,4 +109,26 @@ class GameAITests: XCTestCase {
            XCTAssertTrue(game(board: board, isWonBy: .x))
            XCTAssertFalse(game(board: board, isWonBy: .o))
        }
+    
+    func testWinCheckingDiagnoal1() {
+           //create gameboard object
+           var board = GameBoard()
+           
+           /*              Board Coords, for reference
+               x - -       0,0  1,0  2,0
+               - x -       0,1  1,1  2,1
+               o o x       0,2  1,2  2,2
+            */
+           
+           // make board have that state
+           
+           try! board.place(mark: .x, on: (0,0))
+           try! board.place(mark: .o, on: (0,2))
+           try! board.place(mark: .x, on: (1,1))
+           try! board.place(mark: .o, on: (1,2))
+           try! board.place(mark: .x, on: (2,2))
+
+           XCTAssertTrue(game(board: board, isWonBy: .x))
+           XCTAssertFalse(game(board: board, isWonBy: .o))
+       }
 }
