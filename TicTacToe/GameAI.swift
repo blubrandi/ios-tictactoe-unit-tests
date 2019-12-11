c9fa589c33d9af205ffb4c4b6b908ddb041b8e9b
blubrandi <blubrandi0@gmail.com>
2019-12-11 12:01:47 -0700

    Added testWinCheckingHorizontal1, added for loop in GameAI to increment count of Y marks.  Test passed.

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -13,13 +13,31 @@ func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {
     // how to check that the test we setup for win has particular outcome
     // check verticals
     // refer to 52:15 for explanation of this loop
-    for x in 0..<3 {
+
+    //    for x in 0..<3 {
+//        var numMarks = 0
+//        for y in 0..<3 {
+//            if board[(x,y)] == player {
+//                numMarks += 1
+//            }
+//        }
+//        if numMarks == 3 {
+//            return true
+//        }
+//    }
+    
+    
+    // check horizontals
+    
+    for y in 0..<3 {
         var numMarks = 0
-        for y in 0..<3 {
+        
+        for x in 0..<3 {
             if board[(x,y)] == player {
                 numMarks += 1
             }
         }
+        
         if numMarks == 3 {
             return true
         }
--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -85,4 +85,28 @@ class GameAITests: XCTestCase {
         XCTAssertTrue(game(board: board, isWonBy: .o))
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
+    
+    // Horizontal wins
+    
+    func testWinCheckingHorizontal1() {
+           //create gameboard object
+           var board = GameBoard()
+           
+           /*              Board Coords, for reference
+               x x x       0,0  1,0  2,0
+               o o -       0,1  1,1  2,1
+               - - -       0,2  1,2  2,2
+            */
+           
+           // make board have that state
+           
+           try! board.place(mark: .x, on: (0,0))
+           try! board.place(mark: .o, on: (0,1))
+           try! board.place(mark: .x, on: (1,0))
+           try! board.place(mark: .o, on: (1,1))
+           try! board.place(mark: .x, on: (2,0))
+           
+           XCTAssertTrue(game(board: board, isWonBy: .x))
+           XCTAssertFalse(game(board: board, isWonBy: .o))
+       }
 }
