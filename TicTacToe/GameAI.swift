ab701096dec1544bb3ee4928bcde44352f3e8434
blubrandi <blubrandi0@gmail.com>
2019-12-11 11:26:41 -0700

    Testing when a player wins.  Placed marks and added loop to GameAI to count marks of a passed in player.  Tests pass.

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -9,5 +9,23 @@
 import Foundation
 
 func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {    
+    
+    // how to check that the test we setup for win has particular outcome
+    // check verticals
+    // refer to 52:15 for explanation of this loop
+    for x in 0..<3 {
+        var numMarks = 0
+        for y in 0..<3 {
+            if board[(x,y)] == player {
+                numMarks += 1
+            }
+        }
+        if numMarks == 3 {
+            return true
+        }
+    }
+    
     return false
+    
+
 }
--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -7,9 +7,35 @@
 //
 
 import XCTest
+@testable import TicTacToe
 
 class GameAITests: XCTestCase {
 
-
+    // Test various scenarios where a player might win
+    // X is winning by putting a column on the left side.  We'll make o play the top and middle of the middle column
+    func testWinCheckingVertical1() {
+        // make gameboard object
+        var board = GameBoard()
+        
+        // Below, making a reference to what the game board should look like for reference.
+        /*
+                 x o -
+                 x o -
+                 x - -
+         */
+        
+        // make board have that state
+        
+        try! board.place(mark: .x, on: (0,0))
+        try! board.place(mark: .o, on: (1,0))
+        try! board.place(mark: .x, on: (0,1))
+        try! board.place(mark: .o, on: (1,1))
+        try! board.place(mark: .x, on: (0,2))
+        
+        // Call it
+        // It should be true that X won, and that O lost.
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
 
 }
